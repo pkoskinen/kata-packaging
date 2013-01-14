2e6725475c344e7c0c5e3d08ef6e78044ecc73c6
Uwe Geuder <uwe.geuder@nomovok.com>
2013-01-14 13:25:16 +0200

    rpm: corrections in the SKIP mechanism
    when DB migration failed the log claimed to problem was after SKIP10, but that shouldn't have existed for a long time
    when looking at it I found two more misses

--- tools/internal/devskip.sh
+++ tools/internal/devskip.sh
@@ -1,6 +1,9 @@
 # this is a script template to create /tmp/kata-SKIPnn files
 # to use it comment statements as desired
 
+# *dev*skip refers to development use in general, not to dev.rpm, can also be
+# used with prod.rpm
+
 touch /tmp/kata-SKIP04   # 04configuredependencies.sh
 touch /tmp/kata-SKIP08   # 08getpyenv.sh
 touch /tmp/kata-SKIP12   # 12getpythonpackages.sh
@@ -8,7 +11,10 @@ touch /tmp/kata-SKIP16   # 16configshibbolethsp.sh
 touch /tmp/kata-SKIP20   # 20setuppostgres.sh
 touch /tmp/kata-SKIP24   # 24setupapachessl.sh
 touch /tmp/kata-SKIP28   # 28setupckan.sh
+touch /tmp/kata-SKIP30   # 30setupckanprod.sh
+touch /tmp/kata-SKIP31   # 31setupckan-root.sh
 touch /tmp/kata-SKIP32   # 32setupckanservice.sh
+touch /tmp/kata-SKIP35   # 35setupharvester.sh
 touch /tmp/kata-SKIP36   # 36installckanextensions.sh
 touch /tmp/kata-SKIP40   # 40configsolr.sh
 touch /tmp/kata-SKIP61   # 61setupsources.sh
