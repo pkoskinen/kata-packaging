f4a6a79674014cf8b6929335f23e706e30f0e3d6
Uwe Geuder <uwe.geuder@nomovok.com>
2013-02-21 22:15:32 +0200

    tools: 2 internal test scripts for development work

---
+++ tools/internal/insert-ids-into-test-db.py
@@ -0,0 +1,28 @@
+# reads ids from a file and inserts them into a DB table called test_ids
+#
+# Purpose: Comparing contents of Solr index to contents of DB 
+#
+#          - input file can be created using script get-ids-from-solr.py
+#          - resulting test_ids contents can be compated to package(id) 
+#            using psql
+#
+import sqlalchemy
+sa = sqlalchemy
+import sys
+
+def main(fn):
+    eng = sa.create_engine( 'postgresql://ckanuser:cKan0001@localhost/ckantest')
+    metadata = sa.MetaData()
+    test_ids = sa.Table('test_ids', metadata, sa.Column('id', sa.Text))
+    metadata.create_all(eng)
+    conn = eng.connect()
+    ins = test_ids.insert()
+    infile = open(fn, "r")
+    for line in infile:
+        if line[-1:] == "\n":
+            line = line[:-1]
+        conn. execute(ins, id = line)
+
+
+if __name__ == '__main__' :
+    main(sys.argv[1])
