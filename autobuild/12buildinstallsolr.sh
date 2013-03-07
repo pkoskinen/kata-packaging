30e91c1b4d6d18e695a03679b13869ab855b7f72
Uwe Geuder <uwe.geuder@nomovok.com>
2013-03-07 20:53:27 +0200

    autobuild: allow varying release numbers in solr.rpm

--- autobuild/12buildinstallsolr.sh
+++ autobuild/12buildinstallsolr.sh
@@ -3,7 +3,10 @@ cd rpmbuild/SOURCES
 curl -O http://archive.apache.org/dist/lucene/solr/3.5.0/apache-solr-3.5.0.tgz
 cd ../SPECS
 ln -s ../../rpms/solr/solr.spec
-rpmbuild -ba solr.spec
 arch=$(rpm -q --qf '%{arch}\n' --specfile solr.spec | head -n 1)
+# just in case, make sure the * in yum install command below will not match
+# several files
+rm -f ../RPMS/${arch}/apache-solr-3.5.0-*.el6.${arch}.rpm
+rpmbuild -ba solr.spec
 cd ../RPMS/${arch}
-sudo yum install -y apache-solr-3.5.0-1.el6.${arch}.rpm
+sudo yum install -y apache-solr-3.5.0-*.el6.${arch}.rpm
