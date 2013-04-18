a3dfe501f5cd508d4f9c65d81e0daee92a3499e2
Uwe Geuder <uwe.geuder@nomovok.com>
2013-04-18 12:10:10 +0300

    Revert "INSTALL/autobuild: funet EPEL mirror is empty, use lth.se instead"
    This reverts commit e22f654589950cb5dd9e3984c2971860a90921c1.
    
    funet.fi seems to be OK again

--- autobuild/04addrepos.sh
+++ autobuild/04addrepos.sh
@@ -1,5 +1,4 @@
 #! /bin/sh
-# funet.fi mirror empty on 16-Apr-2013
-sudo yum install -y http://ftp.df.lth.se/pub/fedora-epel/6/x86_64/epel-release-6-8.noarch.rpm
+sudo yum install -y http://www.nic.funet.fi/pub/mirrors/fedora.redhat.com/pub/epel/6/i386/epel-release-6-8.noarch.rpm
 curl -O http://download.opensuse.org/repositories/security://shibboleth/CentOS_CentOS-6/security:shibboleth.repo
 sudo cp security:shibboleth.repo /etc/yum.repos.d/
